17dffdf9edca06f5a8369f471242b6f8223dc5c6
StephanieKrus <stephanie.krus@engineering.digital.dwp.gov.uk>
2024-08-29 17:51:46 +0100

    route all hospital screens

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,26 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+router.post('/part-1/hospital/child-in-hospital', function(request, response) {
+
+    var isInHospital = request.session.data['in-hospital']
+    if (isInHospital === "yes"){
+        response.redirect("/part-1/hospital/went-in-date")
+    } else {
+        response.redirect("/part-1/hospital/overnight-stays")
+    }
+})
+
+router.post('/part-1/hospital/overnight-stays', function(request, response) {
+
+    var hasOvernight = request.session.data['overnight-stay']
+    if (hasOvernight === "yes"){
+        response.redirect("/part-1/hospital/went-in-and-out")
+    } else {
+        response.redirect("/part-1/hospital/end-screen")
+    }
+})
+
+
